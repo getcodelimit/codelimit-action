b891ee8492125eeb618ea1ed8654db0d12d7a8bd
robvanderleek <robvanderleek@users.noreply.github.com>
2024-12-24 14:55:04 +0000

    Distribution build

--- dist/index.js
+++ dist/index.js
@@ -52486,7 +52486,7 @@ var require_version = __commonJS({
     Object.defineProperty(exports2, "__esModule", { value: true });
     exports2.version = void 0;
     exports2.version = {
-      "revision": "61e09ec",
+      "revision": "c977354",
       "year": "2024"
     };
   }
